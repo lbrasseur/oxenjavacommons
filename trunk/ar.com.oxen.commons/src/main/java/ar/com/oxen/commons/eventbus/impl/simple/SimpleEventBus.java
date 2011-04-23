92860af13c41780af5df3a80ca0b734a35b5ddae
lbrasseur <lbrasseur@34f2142c-d99b-ef40-8e76-974750be621f>
2011-04-23 15:14:51 +0000

    event bus fix

--- trunk/ar.com.oxen.commons/src/main/java/ar/com/oxen/commons/eventbus/impl/simple/SimpleEventBus.java
+++ trunk/ar.com.oxen.commons/src/main/java/ar/com/oxen/commons/eventbus/impl/simple/SimpleEventBus.java
@@ -3,28 +3,32 @@ package ar.com.oxen.commons.eventbus.impl.simple;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.WeakHashMap;
 
 import ar.com.oxen.commons.eventbus.api.EventBus;
 import ar.com.oxen.commons.eventbus.api.EventHandler;
 
-
 public class SimpleEventBus implements EventBus {
-	// TODO: Esto deberia ser un WeakHashMap, pero si lo hago asi, a veces lo libera enseguida
-	private Map<EventHandler<?>, Class<?>> handlers =  new HashMap<EventHandler<?>, Class<?>>();
-	
+	private Map<EventHandler<?>, Class<?>> handlers = new HashMap<EventHandler<?>, Class<?>>();
+
 	@SuppressWarnings("unchecked")
-	public void fireEvent(Object event) {
-		for (Entry<EventHandler<?>, Class<?>> entry:handlers.entrySet()) {
+	public synchronized void fireEvent(Object event) {
+		for (Entry<EventHandler<?>, Class<?>> entry : handlers.entrySet()) {
 			if (entry.getValue().equals(event.getClass())) {
-				EventHandler<Object> eventHandler = (EventHandler<Object>) entry.getKey(); 
+				EventHandler<Object> eventHandler = (EventHandler<Object>) entry
+						.getKey();
 				eventHandler.onEvent(event);
 			}
 		}
 	}
 
-	public <T> void addHandler(Class<T> eventClass, EventHandler<T> handler) {
+	public synchronized <T> void addHandler(Class<T> eventClass,
+			EventHandler<T> handler) {
 		this.handlers.put(handler, eventClass);
+	}
+
+	@Override
+	public synchronized void removeHandler(EventHandler<?> handler) {
+		this.handlers.remove(handler);
 	}
 
 }
