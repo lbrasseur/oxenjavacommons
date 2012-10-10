4ec60de777ab081efa2aee3febfc649ebfbda8de
lbrasseur@gmail.com <lbrasseur@gmail.com@34f2142c-d99b-ef40-8e76-974750be621f>
2012-10-10 14:14:08 +0000

    more keystore provider implementations

--- trunk/ar.com.oxen.commons/src/main/java/ar/com/oxen/commons/license/impl/DefaultKeyStoreProvider.java
+++ trunk/ar.com.oxen.commons/src/main/java/ar/com/oxen/commons/license/impl/DefaultKeyStoreProvider.java
@@ -1,38 +1,12 @@
 package ar.com.oxen.commons.license.impl;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.CertificateException;
-
-import ar.com.oxen.commons.license.api.KeyStoreProvider;
-import ar.com.oxen.commons.license.api.LicenceException;
 
 /**
  * Loads the default Java key store.
  */
-public class DefaultKeyStoreProvider implements KeyStoreProvider {
-	@Override
-	public KeyStore getKeyStore() {
-		try {
-			KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
-			ks.load(new FileInputStream(new File(System
-					.getProperty("user.home"), ".keystore")), null);
-			return ks;
-		} catch (KeyStoreException e) {
-			throw new LicenceException(e);
-		} catch (NoSuchAlgorithmException e) {
-			throw new LicenceException(e);
-		} catch (CertificateException e) {
-			throw new LicenceException(e);
-		} catch (FileNotFoundException e) {
-			throw new LicenceException(e);
-		} catch (IOException e) {
-			throw new LicenceException(e);
-		}
+public class DefaultKeyStoreProvider extends FileKeyStoreProvider {
+	public DefaultKeyStoreProvider() {
+		super(new File(System.getProperty("user.home"), ".keystore"));
 	}
 }
